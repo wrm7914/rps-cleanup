c20d92d95d2703437950df39cbfad4381d2005c1
wrm7914 <108708082+wrm7914@users.noreply.github.com>
2022-07-21 19:15:43 -0400

    committing to simple branch - making our own fix here (#1)

--- game.py
+++ game.py
@@ -3,14 +3,14 @@
 
 
 from random import choice
-
+valid_choices = ["rock", "paper", "scissors"]
 #
 # USER SELECTION
 #
 
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in valid_choices:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +18,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_choices)
 print("COMPUTER CHOICE:", c)
 
 #
